3151738d5928bc6e29e5159e7692447ff7af0e14
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-29 20:58:04 -0700

    Add routes to app.jsx; create sign up page and add to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <LandingPage />
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/home" element={<Navigate to="/" />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
