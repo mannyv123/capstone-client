ad281664debcc24c5714d049f2b03cf0e0d1e215
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-29 20:44:50 -0700

    add landing page to app.jsx; update styling on landing page; add in temporary divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <LandingPage />
         </div>
     );
 }
