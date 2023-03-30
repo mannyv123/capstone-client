f9ba8f71e49388b91bb67ee473e5098abc1bbc02
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-30 12:53:03 -0700

    add form and inputs for sign up page; add padding

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,34 @@
 import "./SignUpPage.scss";
 
 function SignUpPage() {
-    return;
+    return (
+        <section className="signup">
+            <h1 className="signup__title">Sign Up</h1>
+            <form action="" className="signup__form">
+                <label htmlFor="username" className="signup__label">
+                    Create username:
+                </label>
+                <input type="text" name="username" id="username" className="signup__input" />
+                <label htmlFor="password" className="signup__label">
+                    Create password:
+                </label>
+                <input type="password" name="password" id="password" className="signup__input" />
+                <label htmlFor="confirmPassword" className="signup__label">
+                    Confirm password:
+                </label>
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    className="signup__input"
+                />
+                <label htmlFor="email" className="signup__label">
+                    Add email:
+                </label>
+                <input type="email" name="email" id="email" className="signup__input" />
+            </form>
+        </section>
+    );
 }
 
 export default SignUpPage;
