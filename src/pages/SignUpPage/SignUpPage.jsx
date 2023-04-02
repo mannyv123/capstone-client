6a639a9eb9ca3f71e36d7f198504746e4a74b782
Manjot Virdi <virdi_manjot@hotmail.com>
2023-04-02 15:23:27 -0700

    add input handler for sign up page

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -4,23 +4,45 @@ import CreateAccount from "../../components/CreateAccount/CreateAccount";
 import CreateProfile from "../../components/CreateProfile/CreateProfile";
 import "./SignUpPage.scss";
 
+const initialValues = {
+    username: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+    profileImg: null,
+    profileImgData: {
+        imgDataUrl: null,
+        imgFilename: "",
+    },
+    name: "",
+    about: "",
+    setup: "",
+};
+
 function SignUpPage() {
     const [step, setStep] = useState("account");
     const navigate = useNavigate();
+    const [values, setValues] = useState(initialValues);
+
+    //Handles updating form input values
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value });
+    };
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        navigate("/");
+        navigate("/profile");
     };
-
+    console.log(values);
     return (
         <section className="signup">
             <h1 className="signup__title">Sign Up</h1>
             <form action="" className="signup__form" onSubmit={handleFormSubmit}>
                 {step === "account" ? (
-                    <CreateAccount setStep={setStep} />
+                    <CreateAccount setStep={setStep} handleInputChange={handleInputChange} />
                 ) : (
-                    <CreateProfile setStep={setStep} />
+                    <CreateProfile setStep={setStep} handleInputChange={handleInputChange} />
                 )}
             </form>
         </section>
