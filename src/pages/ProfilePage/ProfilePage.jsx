f10cbf4f306978264da3c67d92828a207a06c91b
Manjot Virdi <virdi_manjot@hotmail.com>
2023-04-02 21:57:59 -0700

    update sign up and profile pages in order to add profile image and display

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,15 +1,17 @@
 import CollectionsList from "../../components/CollectionsList/CollectionsList";
 import { useParams } from "react-router-dom";
+// import { useState, useEffect } from "react";
 import "./ProfilePage.scss";
 
 function ProfilePage({ currentUser }) {
     const { username } = useParams();
+
     console.log(currentUser);
     return (
         <section className="profile">
             <div className="profile__details">
                 <h1 className="profile__title">Welcome {username}</h1>
-                <img src={currentUser.profileImg} alt="user profile" className="profile__image" />
+                <img src={currentUser.imgDataUrl} alt="user profile" className="profile__image" />
             </div>
             <div className="profile__add-collection">Add New Collection</div>
             <h2 className="profile__collections-title">Your Collections</h2>
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import CreateAccount from "../../components/CreateAccount/CreateAccount";
 import CreateProfile from "../../components/CreateProfile/CreateProfile";
@@ -9,11 +9,6 @@ const initialValues = {
     password: "",
     confirmPassword: "",
     email: "",
-    profileImg: null,
-    profileImgData: {
-        imgDataUrl: null,
-        imgFilename: "",
-    },
     name: "",
     about: "",
     setup: "",
@@ -24,14 +19,49 @@ function SignUpPage({ users, setCurrentUser }) {
     const navigate = useNavigate();
     const [values, setValues] = useState(initialValues);
 
+    const [profileImg, setProfileImg] = useState(null);
+    const [imgDataUrl, setImgDataUrl] = useState(null);
+    // const [imgFilename, setImgFilename] = useState("");
+
+    //useEffect to read the selected file and set the file data url
+    useEffect(() => {
+        let fileReader,
+            isCancel = false;
+        if (profileImg) {
+            fileReader = new FileReader();
+            fileReader.onload = (event) => {
+                const { result } = event.target;
+                if (result && !isCancel) {
+                    setImgDataUrl(result);
+                    // setImgFilename(currentUser.profileImg.name);
+                }
+            };
+            fileReader.readAsDataURL(profileImg);
+        }
+        return () => {
+            isCancel = true;
+            if (fileReader && fileReader.readyState === 1) {
+                fileReader.abort();
+            }
+        };
+    }, [profileImg]);
+
     //Handles updating form input values
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setValues({ ...values, [name]: value });
     };
 
+    //Handles updating for image uploads
+    const handleImageUpload = (event) => {
+        setProfileImg(event.target.files[0]);
+    };
+
     const handleFormSubmit = (event) => {
         event.preventDefault();
+
+        values.imgDataUrl = imgDataUrl;
+        values.profileImg = profileImg;
 
         users.push(values);
         setCurrentUser(values);
@@ -47,7 +77,11 @@ function SignUpPage({ users, setCurrentUser }) {
                 {step === "account" ? (
                     <CreateAccount setStep={setStep} handleInputChange={handleInputChange} />
                 ) : (
-                    <CreateProfile setStep={setStep} handleInputChange={handleInputChange} />
+                    <CreateProfile
+                        setStep={setStep}
+                        handleInputChange={handleInputChange}
+                        handleImageUpload={handleImageUpload}
+                    />
                 )}
             </form>
         </section>
