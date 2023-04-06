43e2211d039cd6f6344923909b9f24804b11d364
Manjot Virdi <virdi_manjot@hotmail.com>
2023-04-05 22:05:20 -0700

    update props to collection list component; add get recent posts api call to landing page and pass data via props to collection list component

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import CollectionsList from "../../components/CollectionsList/CollectionsList";
 import "./LandingPage.scss";
+import axios from "axios";
+import { API_URL } from "../../App";
 
 function LandingPage() {
+    const [recentPostData, setRecentPostData] = useState([]);
+
+    useEffect(() => {
+        getRecentPosts();
+    }, []);
+
+    //Get recent posts
+    async function getRecentPosts() {
+        try {
+            const resp = await axios.get(`${API_URL}/posts`);
+            console.log(resp.data);
+            setRecentPostData(resp.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <section className="landing-page">
             <h1 className="landing-page__title">LATEST COLLECTIONS</h1>
             <div className="landing-page__feed">
-                <CollectionsList />
+                <CollectionsList postsData={recentPostData} showDelete="no" />
             </div>
         </section>
     );
