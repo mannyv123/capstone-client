7ada0087357ea4864621af34a85a6d64ae659291
Manjot Virdi <virdi_manjot@hotmail.com>
2023-04-10 15:16:21 -0700

    update formatting for add collection page, including styling

--- src/pages/AddCollectionPage/AddCollectionPage.jsx
+++ src/pages/AddCollectionPage/AddCollectionPage.jsx
@@ -92,60 +92,81 @@ function AddCollectionPage() {
 
     console.log(currentUser);
     console.log(images);
+    console.log(imagesArray);
     return (
         <section className="new-collection">
             <h1 className="new-collection__title">Add New Collection</h1>
             <form onSubmit={handleFormSubmit} action="" className="new-collection__form">
                 <div className="new-collection__container-imgs">
-                    <h3 className="new-collection__sub-hdr">Select files</h3>
-                    <input type="file" name="images" id="images" multiple onChange={handleImageUploads} />
+                    <h3 className="new-collection__sub-hdr">Select Photos</h3>
+                    <input
+                        className="new-collection__images-input"
+                        type="file"
+                        name="images"
+                        id="images"
+                        multiple
+                        onChange={handleImageUploads}
+                    />
                 </div>
+                <label htmlFor="images" className="new-collection__select-label">
+                    Choose Files
+                </label>
                 <div className="new-collection__container-details">
                     <h3 className="new-collection__sub-hdr">Add Details</h3>
-                    <label htmlFor="title" className="new-collection__input-label">
+                    <label htmlFor="title" className="new-collection__label">
                         Title:
                     </label>
                     <input
-                        className="new-collection__input-title"
+                        className="new-collection__input"
                         type="text"
                         name="title"
                         id="title"
                         onChange={handleInputChange}
                     />
-                    <label htmlFor="description" className="new-collection__input-label">
+                    <label htmlFor="description" className="new-collection__label">
                         Description:
                     </label>
                     <textarea
-                        className="new-collection__input-description"
+                        className="new-collection__input new-collection__input--description"
                         name="description"
                         id="description"
                         onChange={handleInputChange}
                     ></textarea>
                 </div>
                 <div className="new-collection__container-locations">
+                    {imagesArray.length > 0 && <h3 className="new-collection__sub-hdr">Add Photo Details</h3>}
                     {imagesArray.map((image, index) => (
-                        <div>
+                        <div key={index} className="new-collection__image-details">
                             <img src={image} alt="photo" className="new-collection__image-preview" />
-                            <label htmlFor={`imgTitle${index}`}>Image Title:</label>
+                            <label className="new-collection__label" htmlFor={`imgTitle${index}`}>
+                                Image Title:
+                            </label>
                             <input
                                 type="text"
                                 name={`imgTitle${index}`}
                                 id={`imgTitle${index}`}
                                 onChange={(event) => handleImageInfo(event, index)}
+                                className="new-collection__image-input"
                             />
-                            <label htmlFor="imgLat">Latitude:</label>
+                            <label className="new-collection__label" htmlFor="imgLat">
+                                Latitude:
+                            </label>
                             <input
                                 type="text"
                                 name="imgLat"
                                 id="imgLat"
                                 onChange={(event) => handleImageInfo(event, index)}
+                                className="new-collection__image-input new-collection__image-input--location"
                             />
-                            <label htmlFor="imgLong">Longitude:</label>
+                            <label className="new-collection__label" htmlFor="imgLong">
+                                Longitude:
+                            </label>
                             <input
                                 type="text"
                                 name="imgLong"
                                 id="imgLong"
                                 onChange={(event) => handleImageInfo(event, index)}
+                                className="new-collection__image-input new-collection__image-input--location"
                             />
                         </div>
                     ))}
