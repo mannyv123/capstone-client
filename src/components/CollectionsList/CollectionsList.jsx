427e13bad03efcfa6cf13df517f26b9808eb9a9e
Manjot Virdi <virdi_manjot@hotmail.com>
2023-04-11 15:14:27 -0700

    add react-icons package; update module styling to fit image; update modul to click through images

--- src/components/CollectionsList/CollectionsList.jsx
+++ src/components/CollectionsList/CollectionsList.jsx
@@ -6,14 +6,21 @@ import CollectionViewModal from "../CollectionViewModal/CollectionViewModal";
 
 function CollectionsList({ postsData, handleCollectionDelete, showDelete }) {
     const [isOpen, setIsOpen] = useState(false);
+    const [selectedPost, setSelectedPost] = useState({});
     console.log(postsData);
     return (
         <ul className="collections">
-            {isOpen && <CollectionViewModal setIsOpen={setIsOpen} />}
+            {isOpen && <CollectionViewModal setIsOpen={setIsOpen} selectedPost={selectedPost} />}
             {postsData.map((post) => {
                 return (
                     <li className="collections__post" key={post.id}>
-                        <div className="collections__details" onClick={() => setIsOpen(true)}>
+                        <div
+                            className="collections__details"
+                            onClick={() => {
+                                setIsOpen(true);
+                                setSelectedPost(post);
+                            }}
+                        >
                             <h3 className="collections__post-title">{post.title}</h3>
                             <p className="collections__post-description">{post.description}</p>
                             {showDelete === "yes" ? (
@@ -27,7 +34,13 @@ function CollectionsList({ postsData, handleCollectionDelete, showDelete }) {
                                 ""
                             )}
                         </div>
-                        <div className="collections__post-images" onClick={() => setIsOpen(true)}>
+                        <div
+                            className="collections__post-images"
+                            onClick={() => {
+                                setIsOpen(true);
+                                setSelectedPost(post);
+                            }}
+                        >
                             {post.imageUrls.map((image, index) => {
                                 return (
                                     <img
