a2ec1530dcce9877da9642f1a1a1990418520553
Manjot Virdi <virdi_manjot@hotmail.com>
2023-04-11 20:18:19 -0700

    add close icon; add onclick function to close modal for close icon

--- src/components/CollectionViewModal/CollectionViewModal.jsx
+++ src/components/CollectionViewModal/CollectionViewModal.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./CollectionViewModal.scss";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 function CollectionViewModal({ setIsOpen, selectedPost }) {
     const [current, setCurrent] = useState(0);
@@ -23,6 +24,7 @@ function CollectionViewModal({ setIsOpen, selectedPost }) {
         <div className="modal">
             <div className="modal__bg" onClick={() => setIsOpen(false)}></div>
             <div className="modal__body">
+                <IoClose className="modal__close" onClick={() => setIsOpen(false)} />
                 {/* <div className="modal__slider"> */}
                 <IoIosArrowBack className="modal__left-arrow" onClick={prevSlide} />
                 <IoIosArrowForward className="modal__right-arrow" onClick={nextSlide} />
